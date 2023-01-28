a38b3f401a0018a0bd71afd5b8683a4df991e797
Piotr Piech <piotr.piech@nearform.com>
2023-01-29 00:07:20 +0100

    game model and migrations

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,11 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['id'];
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
