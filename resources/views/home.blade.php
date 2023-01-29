b6995871c8bcc708a8d2cb4b1f7d88a8bda7507e
Piotr Piech <piotr.piech@nearform.com>
2023-01-29 08:01:59 +0100

    missing fullstop

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <section class="home">
-    <p>The goal of this game is to guess the current temperature in randomly selected cities from all over the world with population of at least 200,000</p>
+    <p>The goal of this game is to guess the current temperature in randomly selected cities from all over the world with population of at least 200,000.</p>
     <form method="POST" action="{{ route('play.store') }}">
         @csrf
         <button type="submit" class="btn btn-secondary">New game</button>
